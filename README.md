13a6bf0523c241a127fe6be664b85bad0bd44e10
L. Wagner <lion.wagner@freenet.de>
2023-01-19 17:05:09 +0100

    switch to unittest

--- README.md
+++ README.md
@@ -148,6 +148,98 @@ FAILED tests/unit/test_diffusion2d_functions.py::test_set_initial_condition - as
 
 ### unittest log
 
+```
+Launching unittests with arguments python -m unittest D:/GitHub/SSE/testing-python-exercise-wt2223/tests/unit/test_diffusion2d_functions.py in D:\GitHub\SSE\testing-python-exercise-wt2223\tests\unit
+
+
+
+111.0 != 37
+
+Expected :37
+Actual   :111.0
+<Click to see difference>
+
+Traceback (most recent call last):
+  File "D:\Programme & Funktionen\PyCharm Community Edition 2019.2.2\helpers\pycharm\teamcity\diff_tools.py", line 32, in _patched_equals
+    old(self, first, second, msg)
+  File "D:\Programme & Funktionen\Python3\lib\unittest\case.py", line 839, in assertEqual
+    assertion_func(first, second, msg=msg)
+  File "D:\Programme & Funktionen\Python3\lib\unittest\case.py", line 832, in _baseAssertEqual
+    raise self.failureException(msg)
+AssertionError: 37 != 111.0
+
+During handling of the above exception, another exception occurred:
+
+Traceback (most recent call last):
+  File "D:\Programme & Funktionen\Python3\lib\unittest\case.py", line 59, in testPartExecutor
+    yield
+  File "D:\Programme & Funktionen\Python3\lib\unittest\case.py", line 615, in run
+    testMethod()
+  File "D:\GitHub\SSE\testing-python-exercise-wt2223\tests\unit\test_diffusion2d_functions.py", line 28, in test_initialize_domain
+    self.assertEqual(self.solver.nx, w // dx)
+
+dt = 0.0002837885791219549
+D:\GitHub\SSE\testing-python-exercise-wt2223\tests\unit\test_diffusion2d_functions.py:56: DeprecationWarning: Please use assertAlmostEqual instead.
+  self.assertAlmostEquals(self.solver.dt, dt, 2)
+dt = 0.0001524399938569907
+dt = 2.0613416973528867e-05
+dt = 0.0002049174953392603
+dt = 0.00014924574560637533
+dt = 9.995065225381505e-05
+dt = 0.0001798893416755572
+dt = 0.00011597342969159412
+dt = 0.00016356510992831505
+dt = 0.0003962470475809662
+
+
+Ran 3 tests in 0.030s
+
+FAILED (failures=2)
+
+
+700.0 != 490000.0
+
+Expected :490000.0
+Actual   :700.0
+<Click to see difference>
+
+Traceback (most recent call last):
+  File "D:\Programme & Funktionen\PyCharm Community Edition 2019.2.2\helpers\pycharm\teamcity\diff_tools.py", line 32, in _patched_equals
+    old(self, first, second, msg)
+  File "D:\Programme & Funktionen\Python3\lib\unittest\case.py", line 839, in assertEqual
+    assertion_func(first, second, msg=msg)
+  File "D:\Programme & Funktionen\Python3\lib\unittest\case.py", line 832, in _baseAssertEqual
+    raise self.failureException(msg)
+AssertionError: 490000.0 != 700.0
+
+During handling of the above exception, another exception occurred:
+
+Traceback (most recent call last):
+  File "D:\Programme & Funktionen\Python3\lib\unittest\case.py", line 59, in testPartExecutor
+    yield
+  File "D:\Programme & Funktionen\Python3\lib\unittest\case.py", line 615, in run
+    testMethod()
+  File "D:\GitHub\SSE\testing-python-exercise-wt2223\tests\unit\test_diffusion2d_functions.py", line 77, in test_set_initial_condition
+    self.assertEqual(u.max(), self.solver.T_hot)
+
+
+Assertion failed
+
+Assertion failed
+
+Process finished with exit code 1
+
+Assertion failed
+
+Assertion failed
+
+Assertion failed
+
+Assertion failed
+
+```
+
+
 ## Citing
 
 The code used in this exercise is based on [Chapter 7 of the book "Learning Scientific Programming with Python"](https://scipython.com/book/chapter-7-matplotlib/examples/the-two-dimensional-diffusion-equation/).
--- tests/unit/test_diffusion2d_functions.py
+++ tests/unit/test_diffusion2d_functions.py
@@ -4,73 +4,78 @@ Tests for functions in class SolveDiffusion2D
 
 from diffusion2d import SolveDiffusion2D
 import numpy as np
-import pytest
+import unittest
 
 
-def test_initialize_domain():
-    """
-    Check function SolveDiffusion2D.initialize_domain
-    """
-    solver = SolveDiffusion2D()
+class TestDiffusion2D(unittest.TestCase):
 
-    for i in range(1000):
-        w = np.random.rand() * 100
-        h = np.random.rand() * 100
-        dx = np.random.rand()
-        dy = np.random.rand()
-        solver.initialize_domain(w=w, h=h, dx=dx, dy=dy)
-        assert solver.nx == w // dx
-        assert solver.ny == h // dy
+    def setUp(self) -> None:
+        self.solver = SolveDiffusion2D()
+
+        return super().setUp()
+
+    def test_initialize_domain(self):
+        """
+        Check function SolveDiffusion2D.initialize_domain
+        """
+
+        for i in range(10):
+            w = np.random.rand() * 100
+            h = np.random.rand() * 100
+            dx = np.random.rand()
+            dy = np.random.rand()
+            self.solver.initialize_domain(w=w, h=h, dx=dx, dy=dy)
+            self.assertEqual(self.solver.nx, w // dx)
+            self.assertEqual(self.solver.ny, h // dy)
+
+    def test_initialize_physical_parameters(self):
+        """
+        Checks function SolveDiffusion2D.initialize_domain
+        """
+        self.solver.w = 10.
+        self.solver.h = 10.
+        self.solver.dx = 0.1
+        self.solver.dy = 0.1
+        self.solver.nx = 100
+        self.solver.ny = 100
+
+        dx2, dy2 = self.solver.dx ** 2, self.solver.dy**2
+
+        for i in range(10):
+            d = np.random.rand() * 100
+            T_cold = np.random.rand() * 10000
+            T_hot = np.random.rand() * 10000
+            self.solver.initialize_physical_parameters(
+                d=d, T_cold=T_cold, T_hot=T_hot)
+
+            self.assertEqual(self.solver.D, d)
+            self.assertEqual(self.solver.T_cold, T_cold)
+            self.assertEqual(self.solver.T_hot, T_hot)
+
+            dt = dx2 * dy2 / (2 * self.solver.D * (dx2 + dy2))
+            self.assertAlmostEquals(self.solver.dt, dt, 2)
+
+    def test_set_initial_condition(self):
+        """
+        Checks function SolveDiffusion2D.get_initial_function
+        """
+
+        self.solver.w = 10.
+        self.solver.h = 10.
+        self.solver.dx = 0.1
+        self.solver.dy = 0.1
+        self.solver.nx = 100
+        self.solver.ny = 100
+        self.solver.D = 4.
+        self.solver.T_cold = 300.
+        self.solver.T_hot = 700.
+        self.solver.dt = 0.000625
+
+        u = self.solver.set_initial_condition()
+        self.assertEqual(u.shape, (100, 100))
+        self.assertEqual(u.min(), self.solver.T_cold)
+        self.assertEqual(u.max(), self.solver.T_hot)
 
 
-def test_initialize_physical_parameters():
-    """
-    Checks function SolveDiffusion2D.initialize_domain
-    """
-    solver = SolveDiffusion2D()
-
-    solver.w = 10.
-    solver.h = 10.
-    solver.dx = 0.1
-    solver.dy = 0.1
-    solver.nx = 100
-    solver.ny = 100
-
-    dx2, dy2 = solver.dx ** 2, solver.dy**2
-
-    for i in range(1000):
-        d = np.random.rand() * 100
-        T_cold = np.random.rand() * 10000
-        T_hot = np.random.rand() * 10000
-        solver.initialize_physical_parameters(d=d, T_cold=T_cold, T_hot=T_hot)
-        assert solver.D == d
-        assert solver.T_cold == T_cold
-        assert solver.T_hot == T_hot
-
-        dt = pytest.approx(dx2 * dy2 / (2 * solver.D * (dx2 + dy2)), 0.01)
-        assert solver.dt == dt
-
-
-def test_set_initial_condition():
-    """
-    Checks function SolveDiffusion2D.get_initial_function
-    """
-    solver = SolveDiffusion2D()
-
-    solver.w = 10.
-    solver.h = 10.
-    solver.dx = 0.1
-    solver.dy = 0.1
-    solver.nx = 100
-    solver.ny = 100
-
-    solver.D = 4.
-    solver.T_cold = 300.
-    solver.T_hot = 700.
-    solver.dt = 0.000625
-
-    u = solver.set_initial_condition()
-    assert u.shape == (100, 100)
-    assert u.min() == solver.T_cold
-    assert u.max() == solver.T_hot
-    print(u)
+if __name__ == '__main__':
+    unittest.main()
