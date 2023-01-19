88227661b7d8d5e6f7eb22e23821a306394da3b3
Nils Imhoff <nils@d4d1.dev>
2023-01-19 16:32:02 +0100

    add step 3

--- tests/integration/test_diffusion2d.py
+++ tests/integration/test_diffusion2d.py
@@ -10,6 +10,9 @@ def test_initialize_physical_parameters():
     Checks function SolveDiffusion2D.initialize_domain
     """
     solver = SolveDiffusion2D()
+    solver.initialize_physical_parameters()
+
+
 
 
 def test_set_initial_condition():
--- tests/unit/test_diffusion2d_functions.py
+++ tests/unit/test_diffusion2d_functions.py
@@ -1,7 +1,7 @@
 """
 Tests for functions in class SolveDiffusion2D
 """
-
+import pytest
 from diffusion2d import SolveDiffusion2D
 
 
@@ -10,6 +10,15 @@ def test_initialize_domain():
     Check function SolveDiffusion2D.initialize_domain
     """
     solver = SolveDiffusion2D()
+    solver.initialize_domain(w=5., h=5., dx=0.2, dy=0.2)
+
+    assert solver.w == 5.
+    assert solver.h == 5.
+    assert solver.dx == 0.2
+    assert solver.dy == 0.2
+    assert solver.nx == 25
+    assert solver.ny == 25
+    
 
 
 def test_initialize_physical_parameters():
@@ -17,6 +26,13 @@ def test_initialize_physical_parameters():
     Checks function SolveDiffusion2D.initialize_domain
     """
     solver = SolveDiffusion2D()
+    solver.initialize_domain()
+    solver.initialize_physical_parameters(d=2., T_cold=200., T_hot=500.)
+
+    assert solver.D == 2.
+    assert solver.T_cold == 200.
+    assert solver.T_hot == 500.
+    assert  pytest.approx(solver.dt, 0.0001) == 0.00125
 
 
 def test_set_initial_condition():
@@ -24,3 +40,8 @@ def test_set_initial_condition():
     Checks function SolveDiffusion2D.get_initial_function
     """
     solver = SolveDiffusion2D()
+    solver.initialize_domain()
+    solver.initialize_physical_parameters()
+    actual = solver.set_initial_condition()
+
+    assert actual[0, 0] == 300.
