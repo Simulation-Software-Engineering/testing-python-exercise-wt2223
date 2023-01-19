96351827f2c73d0b7c2171c5ff2b1d41b145bb08
Johannes Erwerle <jo@swagspace.org>
2023-01-19 16:40:59 +0100

    replaced identical parameters in the unittests

--- tests/unit/test_diffusion2d_functions.py
+++ tests/unit/test_diffusion2d_functions.py
@@ -13,10 +13,10 @@ def test_initialize_domain():
     Check function SolveDiffusion2D.initialize_domain
     """
     solver = SolveDiffusion2D()
-    solver.initialize_domain(w=20., h=15., dx=0.1, dy=0.1)
+    solver.initialize_domain(w=20., h=15., dx=0.1, dy=0.2)
 
     assert solver.nx == 200
-    assert solver.ny == 150
+    assert solver.ny == 75
 
 
 def test_initialize_physical_parameters():
@@ -24,11 +24,11 @@ def test_initialize_physical_parameters():
     Checks function SolveDiffusion2D.initialize_domain
     """
     solver = SolveDiffusion2D()
-    solver.dx = 0.1
+    solver.dx = 0.2
     solver.dy = 0.1
     solver.initialize_physical_parameters(d=5., T_cold=400.0, T_hot=600.0)
 
-    assert solver.dt == approx(0.0005)
+    assert solver.dt == approx(0.0008)
 
 
 def test_set_initial_condition():
@@ -40,15 +40,15 @@ def test_set_initial_condition():
     solver.T_cold = 400.0
     solver.T_hot = 600.0
 
-    # this results in a 5x5 grid with the lower-most cell containing the center
+    # this results in a 6x5 grid with the lower-most cell containing the center
     # of the circle
-    solver.nx = 5
+    solver.nx = 6
     solver.ny = 5
     solver.dx = 1.25
-    solver.dy = 1.25
+    solver.dy = 1.5
 
-    expected = 400.0 * np.ones((5, 5))
-    for i in range(3, 5):
+    expected = 400.0 * np.ones((6, 5))
+    for i in range(3, 6):
         for j in range(3, 5):
             expected[i][j] = 600.0
 
